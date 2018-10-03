c3db0d5652d2b593d02f9abc7a42d0366cac8fae
Ludo81 <ludovic.tournier@hotmail.fr>
2018-10-03 18:09:04 +0200

    Mise en ligne

--- src/main/java/simplejdbc/CustomerEntity.java
+++ src/main/java/simplejdbc/CustomerEntity.java
@@ -4,43 +4,74 @@ package simplejdbc;
  * Correspond à un enregistrement de la table Customer
  */
 public class CustomerEntity {
-	// TODO : ajouter les autres propriétés
-	private int customerId;
-	private String name;
-	private String addressLine1;
+    
+	private int customerId, zip, credit_limit;
+        private char discount_code;
+	private String name, addressLine1, adressLine2, city, state, phone, fax, email;
 
-	public CustomerEntity(int customerId, String name, String addressLine1) {
-		this.customerId = customerId;
-		this.name = name;
-		this.addressLine1 = addressLine1;
+	public CustomerEntity(int customerId, int zip, int credit_limit,
+                char discount_code,
+                String name, String addressLine1, String adressLine2,
+                String city, String state, String phone, String fax, String email) {
+                    this.customerId = customerId;
+                    this.zip = zip;
+                    this.credit_limit = credit_limit;
+                    this.discount_code = discount_code;
+                    this.name = name;
+                    this.addressLine1 = addressLine1;
+                    this.adressLine2 = adressLine2;
+                    this.city = city;
+                    this.state = state;
+                    this.phone = phone;
+                    this.fax= fax;
+                    this.email = email;
 	}
 
-	/**
-	 * Get the value of customerId
-	 *
-	 * @return the value of customerId
-	 */
 	public int getCustomerId() {
-		return customerId;
+            return customerId;
 	}
-
-	/**
-	 * Get the value of name
-	 *
-	 * @return the value of name
-	 */
+        
+        public int getZip(){
+            return zip;
+        }
+        
+        public int getCredit_limit() {
+            return credit_limit;
+        }
+        
+        public char getDiscount_code() {
+            return discount_code;
+        }
+        
 	public String getName() {
-		return name;
+            return name;
 	}
 
-	/**
-	 * Get the value of addressLine1
-	 *
-	 * @return the value of addressLine1
-	 */
 	public String getAddressLine1() {
-		return addressLine1;
+            return addressLine1;
 	}
 
-
+        public String getAdressLine2(){
+            return adressLine2;
+        }
+        
+        public String getCity() {
+            return city;
+        }
+        
+        public String getState() {
+            return state;
+        }
+        
+        public String getPhone() {
+            return phone;
+        }
+        
+        public String getFax() {
+            return fax;
+        }
+        
+        public String getEmail() {
+            return email;
+        }
 }
--- src/main/java/simplejdbc/DAO.java
+++ src/main/java/simplejdbc/DAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -80,8 +81,26 @@ public class DAO {
 	 * @throws DAOException
 	 */
 	public int numberOfOrdersForCustomer(int customerId) throws DAOException {
-		throw new UnsupportedOperationException("Pas encore implémenté");
-	}
+		int result = 0;
+
+		String sql = "SELECT COUNT(*) AS NUMBER FROM APP.PURCHASE_ORDER WHERE CUSTOMER_ID = ?";
+		// Syntaxe "try with resources" 
+		// cf. https://stackoverflow.com/questions/22671697/try-try-with-resources-and-connection-statement-and-resultset-closing
+		try (Connection connection = myDataSource.getConnection(); // Ouvrir une connexion
+			Statement stmt = connection.createStatement(); // On crée un statement pour exécuter une requête
+			ResultSet rs = stmt.executeQuery(sql) // Un ResultSet pour parcourir les enregistrements du résultat
+			) {
+			if (rs.next()) { // Pas la peine de faire while, il y a 1 seul enregistrement
+				// On récupère le champ NUMBER de l'enregistrement courant
+				result = rs.getInt("NUMBER");
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
+			throw new DAOException(ex.getMessage());
+		}
+
+		return result;
+        }
 
 	/**
 	 * Trouver un Customer à partir de sa clé
@@ -91,7 +110,22 @@ public class DAO {
 	 * @throws DAOException
 	 */
 	CustomerEntity findCustomer(int customerID) throws DAOException {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+            Object result = new Object();
+
+		String sql = "SELECT * FROM CUSTOMER WHERE CUSTOMER_ID = ?";
+
+		try (Connection connection = myDataSource.getConnection(); // Ouvrir une connexion
+			Statement stmt = connection.createStatement(); // On crée un statement pour exécuter une requête
+			ResultSet rs = stmt.executeQuery(sql) // Un ResultSet pour parcourir les enregistrements du résultat
+			) { 
+			result = rs.getObject(1);
+               
+		} catch (SQLException ex) {
+			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
+			throw new DAOException(ex.getMessage());
+		}
+
+		return (CustomerEntity) result;
 	}
 
 	/**
@@ -102,7 +136,22 @@ public class DAO {
 	 * @throws DAOException
 	 */
 	List<CustomerEntity> customersInState(String state) throws DAOException {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+            List<CustomerEntity> result = new ArrayList<CustomerEntity>();
+
+		String sql = "SELECT * FROM CUSTOMER WHERE STATE = ?";
+
+		try (Connection connection = myDataSource.getConnection(); // Ouvrir une connexion
+			Statement stmt = connection.createStatement(); // On crée un statement pour exécuter une requête
+			ResultSet rs = stmt.executeQuery(sql) // Un ResultSet pour parcourir les enregistrements du résultat
+			) {
+                         result.add((CustomerEntity) rs.getObject(1));
+
+		} catch (SQLException ex) {
+			Logger.getLogger("DAO").log(Level.SEVERE, null, ex);
+			throw new DAOException(ex.getMessage());
+		}
+
+		return result;
 	}
 
 }
